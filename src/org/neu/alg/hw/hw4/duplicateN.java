85656d0bb69e4248683a92e9eb0bd69308d6ee21
001027261_Shunfang Lan <lanshunfang@gmail.com>
2020-02-06 20:04:24 -0800

    [#12] [Duplication] Add alg_nsquare_time_constant_space

--- src/org/neu/alg/hw/hw4/duplicateN.java
+++ src/org/neu/alg/hw/hw4/duplicateN.java
@@ -1,26 +1,52 @@
 package org.neu.alg.hw.hw4;
+
 import org.neu.alg.hw.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * File Name: duplicateN.java
  * duplicateN concrete class
- *
+ * <p>
  * To Compile: IntUtil.java RandomInt.java duplicateN.java duplicateNBase.java
  *
  * @author Jagadeesh Vasudevamurthy
  * @year 2019
  */
 
-class duplicateN extends duplicateNBase{
+class duplicateN extends duplicateNBase {
   duplicateN() {
     //NOTHING CAN BE CHANGED HERE
-    testBed() ;
+    testBed();
   }
 
 
   private int alg_nsquare_time_constant_space() {
-    TBDException tbdException = new TBDException("alg_nsquare_time_constant_space");
-    return 1;
+//    TBDException tbdException = new TBDException("alg_nsquare_time_constant_space");
+    /**
+     * @example
+     * arr == {0, 1, 2, 2, 3, 3, 2, 1, 4}
+     * assert(maxElementValue(arr)).isLessEqualThan(arr.length - 1)
+     */
+
+    List<int[]> targetListToCheckDuplication = Arrays.asList(this.a);
+
+    return (int) targetListToCheckDuplication
+        .stream()
+        .filter(
+            (ele) -> {
+              return targetListToCheckDuplication
+                  .stream()
+                  .anyMatch(
+                      (eleInner) -> {
+                        return eleInner == ele;
+                      }
+                  );
+            }
+        )
+        .count();
+
   }
 
   private int alg_ntime_n_space() {
@@ -42,7 +68,7 @@ class duplicateN extends duplicateNBase{
   @Override
   protected int ntime_n_space() {
     //NOTHING CAN BE CHANGED HERE
-    return alg_ntime_n_space() ;
+    return alg_ntime_n_space();
   }
 
   @Override
@@ -55,11 +81,10 @@ class duplicateN extends duplicateNBase{
   //YOU WRITE YOUR CODE BELOW. DO NOT CRAM entire code in one procedure
 
 
-
   public static void main(String[] args) {
     //NOTHING CAN BE CHANGED HERE
     System.out.println("duplicateN problem STARTS");
-    duplicateN m = new duplicateN() ;
+    duplicateN m = new duplicateN();
     System.out.println("You now understand why we should reduce O(n^2) algorithm to O(n) algorithm");
     System.out.println("duplicateN problem ENDS");
   }
