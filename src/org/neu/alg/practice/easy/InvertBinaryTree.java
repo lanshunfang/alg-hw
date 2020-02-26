ca492ae685e7c273f3a6004fc2121758ecfb62ba
001027261_Shunfang Lan <lanshunfang@gmail.com>
2020-02-25 19:40:48 -0800

    [#19] Invert binary tree

--- src/org/neu/alg/practice/easy/InvertBinaryTree.java
+++ src/org/neu/alg/practice/easy/InvertBinaryTree.java
@@ -6,15 +6,44 @@ import org.neu.alg.practice.lib.data_types.*;
 public class InvertBinaryTree {
   private void invert(BinaryTree binaryTree) {
 
+    BinaryTreeNode rootNode = binaryTree.root;
+    this.invertRecursive(rootNode);
+
+
+  }
+
+  private void invertRecursive(BinaryTreeNode node) {
+
+    if (node == null) {
+      return;
+    }
+
+    BinaryTreeNode leftNode = node.leftTreeNode;
+    node.leftTreeNode = node.rightTreeNode;
+    node.rightTreeNode = leftNode;
+
+    if (node.leftTreeNode != null) {
+      this.invertRecursive(node.leftTreeNode);
+    }
+    if (node.rightTreeNode != null) {
+      this.invertRecursive(node.rightTreeNode);
+    }
+
   }
 
   public static void main(String... args) {
     InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
 
     BinaryTree binaryTree = BinaryTreeUtil.generateIntBinaryTree(3, 100);
+
+    System.out.println("[INFO] Before invert");
     BinaryTreeUtil.display(binaryTree);
 
     invertBinaryTree.invert(binaryTree);
+
+    System.out.println("[INFO] After invert");
+    BinaryTreeUtil.display(binaryTree);
+
 
   }
 }
