fd4d089a059bf76a9b785e980a21923c20a0056b
001027261_Shunfang Lan <lanshunfang@gmail.com>
2020-03-10 13:39:20 -0700

    [#26] HW7: Init data

---
+++ src/org/neu/alg/hw/hw7/GraphBuilder.java
@@ -0,0 +1,36 @@
+package org.neu.alg.hw.hw7;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+/**
+ * File Name: GraphBuilder.java 
+ * All routines that builds Graph
+ * 
+ * @author Jagadeesh Vasudevamurthy
+ * @year 2019
+ */
+
+/*
+ * To compile you require: IntUtil.java RandomInt.java Graph.java GraphTest.javs GraphBuilder.java
+ */
+
+class GraphBuilder{
+	private Graph g ;
+	//You can have any number of private variables and private funcions
+	
+	GraphBuilder(Graph g, String f) {
+		this.g = g ;
+		//WRITE YOUR CODE
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("GraphBuilder.java starts");
+		System.out.println("Use GraphTester.java to test");
+		System.out.println("GraphBuilder.java Ends");
+	}
+}
---
+++ src/org/neu/alg/hw/hw7/GraphDot.java
@@ -0,0 +1,36 @@
+package org.neu.alg.hw.hw7;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+/**
+ * File Name: GraphDot.java 
+ * Writes graph as a dot file
+ * 
+ * @author Jagadeesh Vasudevamurthy
+ * @year 2019s
+ */
+
+/*
+ * To compile you require: IntUtil.java RandomInt.java Graph.java GraphTest.javs GraphDot.java
+ */
+
+class GraphDot{
+	private Graph g ;
+	private String fname;
+	//You can have any number of private variables
+
+	GraphDot(Graph g, String s) {
+		this.g = g ;
+		this.fname = s ;
+		//WRITE CODE
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("GraphDot.java starts");
+		System.out.println("Use GraphTester.java to test");
+		System.out.println("GraphDot.java Ends");
+	}
+}
