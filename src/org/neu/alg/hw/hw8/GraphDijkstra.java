510b20b407394fd3a494afcea7e9e8c3ca397305
001027261_Shunfang Lan <lanshunfang@gmail.com>
2020-03-25 10:29:39 -0700

    [#36] Init questions

---
+++ src/org/neu/alg/hw/hw8/GraphDijkstra.java
@@ -0,0 +1,44 @@
+package org.neu.alg.hw.hw8;
+
+
+/**
+ * File Name: GraphDijkstra.java 
+ * Implements Dijkstra's algorithms
+ * 
+ * @author Jagadeesh Vasudevamurthy
+ * @year 2019
+ */
+
+/*
+ * To compile you require: IntUtil.java RandomInt.java Graph.java GraphTest.javs GraphDijkstra.java
+ */
+
+class GraphDijkstra{
+	//You can have any number of private variables and private functions
+	//DATA GIVEN
+	private String t ; //Title
+	private String dotFile; //Write tree as a dot file
+	private Graph g ;
+	String start;
+	String end;
+	int [] work ;
+	double [] cost ;
+	
+
+	GraphDijkstra(String t, String dotFile, Graph g, String start, String end, int [] work, double [] cost) {
+		this.t = t ;
+		this.dotFile = dotFile ;
+		this.g = g ;
+		this.start = start;
+		this.end = end ;
+		this.work = work ;
+		this.cost = cost ;
+		//WRITE YOUR CODE
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("GraphDijkstra.java starts");
+		System.out.println("Use GraphTest.java to test");
+		System.out.println("GraphDijkstra.java Ends");
+	}
+}
---
+++ src/org/neu/alg/hw/hw8/GraphDot.java
@@ -0,0 +1,101 @@
+package org.neu.alg.hw.hw8;
+
+import org.neu.alg.hw.IntUtil;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+
+
+/**
+ * File Name: GraphDot.java
+ * Writes graph as a dot file
+ *
+ * @author Jagadeesh Vasudevamurthy
+ * @year 2019s
+ */
+
+/*
+ * To compile you require: IntUtil.java RandomInt.java Graph.java GraphTest.javs GraphDot.java
+ */
+
+public class GraphDot {
+  private Graph g;
+  private String fname;
+  private IntUtil u = new IntUtil();
+
+  //You can have any number of private variables
+  // https://dreampuf.github.io/GraphvizOnline/#digraph%20G%20%7B%0A%0A%20%20subgraph%20cluster_0%20%7B%0A%20%20%20%20style%3Dfilled%3B%0A%20%20%20%20color%3Dlightgrey%3B%0A%20%20%20%20node%20%5Bstyle%3Dfilled%2Ccolor%3Dwhite%5D%3B%0A%20%20%20%20a0%20-%3E%20a1%20-%3E%20a2%20-%3E%20a3%3B%0A%20%20%20%20label%20%3D%20%22process%20%231%22%3B%0A%20%20%7D%0A%0A%20%20subgraph%20cluster_1%20%7B%0A%20%20%20%20node%20%5Bstyle%3Dfilled%5D%3B%0A%20%20%20%20b0%20-%3E%20b1%20-%3E%20b2%20-%3E%20b3%3B%0A%20%20%20%20label%20%3D%20%22process%20%232%22%3B%0A%20%20%20%20color%3Dblue%0A%20%20%7D%0A%20%20start%20-%3E%20a0%3B%0A%20%20start%20-%3E%20b0%3B%0A%20%20a1%20-%3E%20b3%3B%0A%20%20b2%20-%3E%20a3%3B%0A%20%20a3%20-%3E%20a0%3B%0A%20%20a3%20-%3E%20end%3B%0A%20%20b3%20-%3E%20end%3B%0A%0A%20%20start%20%5Bshape%3DMdiamond%5D%3B%0A%20%20end%20%5Bshape%3DMsquare%5D%3B%0A%7D
+
+  GraphDot(Graph g, String s) {
+    this.g = g;
+    this.fname = s;
+    //WRITE CODE
+
+    GraphTest.GraphType graphType = g.getType();
+    boolean isDirected = graphType == GraphTest.GraphType.DIRECTED || graphType == GraphTest.GraphType.WEIGHTED_DIRECTED;
+
+    u.openDotFile(s);
+
+    int gnumV = g.getnumV();
+
+    HashMap hmReduceUndirectedDuplication = new HashMap<String, Boolean>();
+
+    String directSymbol = "->";
+
+    for (int i = 0; i < gnumV; ++i) {
+      String n = g.getRealName(i);
+      {
+        int f = g.numFanout(i);
+        if (f == 0) {
+//					System.out.println("NONE");
+        } else {
+          for (int j = 0; j < f; ++j) {
+//						++numedge;
+            int fo = g.getNodeFanout(i, j);
+            String nf = g.getRealName(fo);
+
+            String edgeExpr = n + directSymbol + nf;
+            String edgeExprReverse = nf + directSymbol + n;
+
+            if (!isDirected) {
+              if (
+                  hmReduceUndirectedDuplication.containsKey(edgeExpr)
+                      && hmReduceUndirectedDuplication.get(edgeExpr).equals(true)
+              ) {
+                continue;
+              }
+
+              hmReduceUndirectedDuplication.put(edgeExprReverse, true);
+            }
+
+            DecimalFormat df = new DecimalFormat("#.#");
+
+            String weightLabel = graphType == GraphTest.GraphType.WEIGHTED_UNDIRECTED
+                || graphType == GraphTest.GraphType.WEIGHTED_DIRECTED ? String.format("label=\"%s\", ", df.format(g.getNodeFanoutEdgeWeight(i, j)))
+                : "";
+
+            String extraLabel = String.format(
+                "[%s dir=\"%s\", color=\"red\"]",
+                weightLabel,
+                isDirected ? "forward" : "none"
+            );
+
+            u.appendDotFile(
+                s,
+                n + " " + directSymbol + " " + nf + extraLabel + ";"
+            );
+
+          }
+        }
+      }
+    }
+    u.closeDotFile(s);
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println("GraphDot.java starts");
+    System.out.println("Use GraphTester.java to test");
+    System.out.println("GraphDot.java Ends");
+  }
+}
